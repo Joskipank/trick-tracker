22ec6ef3b29be60812d81c45a839b2a978219ce9
Joskipank <magaiminov8@gmail.com>
2026-05-03 23:47:07 +0300

    feat(user-service): add profile DTOs and repository

--- user-service/src/main/java/com/tricktracker/userservice/dto/response/ProfileResponse.java
+++ user-service/src/main/java/com/tricktracker/userservice/dto/response/ProfileResponse.java
@@ -1,4 +1,22 @@
-package com.tricktracker.userservice.dto.responce;
+package com.tricktracker.userservice.dto.response;
 
-public class ProfileResponce {
+import com.tricktracker.userservice.enums.SkateLevel;
+import lombok.*;
+import com.tricktracker.userservice.enums.VisibilityLevel;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class ProfileResponse {
+    private String username;
+    private String firstName;
+    private String lastName;
+    private String bio;
+    private String avatarSvg;
+    private String phone;
+    private String email;
+    private VisibilityLevel phoneVisibility;
+    private SkateLevel skateLevel;
 }
