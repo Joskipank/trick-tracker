e95a21ecd2ed6d246259858a224914febbdb9e46
Joskipank <magaiminov8@gmail.com>
2026-05-05 09:29:40 +0300

    test: add unit test for user-service

--- user-service/src/test/java/com/tricktracker/userservice/UserProfileControllerTest.java
+++ user-service/src/test/java/com/tricktracker/userservice/UserProfileControllerTest.java
@@ -1,8 +1,8 @@
 package com.tricktracker.userservice;
 
+import com.tricktracker.userservice.dto.response.ProfileResponse;
 import com.tricktracker.userservice.entity.UserProfileEntity;
 import com.tricktracker.userservice.repository.UserProfileRepository;
-import com.tricktracker.userservice.service.UserProfileService;
 import com.tricktracker.userservice.service.impl.UserProfileServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -10,8 +10,14 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+
+import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
 @ExtendWith(MockitoExtension.class)
-public class UserControllerTest {
+public class UserProfileControllerTest {
     @Mock
     private UserProfileRepository userProfileRepository;
 
@@ -24,6 +30,13 @@ public class UserControllerTest {
         UserProfileEntity entity = new UserProfileEntity();
         entity.setUsername(username);
 
-        whem()
+        when(userProfileRepository.findByUsername(username)).thenReturn(Optional.of(entity));
+        ProfileResponse response = userProfileService.getProfileByUsername(username);
+
+        assertNotNull(response);
+        assertEquals("tony_hawk", response.getUsername());
+
+        // Проверяем, что сервис вообще ходил в репозиторий 1 раз
+        verify(userProfileRepository, times(1)).findByUsername(username);
     }
 }
