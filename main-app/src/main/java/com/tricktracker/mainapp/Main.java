312ddef1194d90526e4134d1206b228f5449377f
Ваше Имя ÐÐMuhammed <magaiminov8@gmail.com>
2026-09-09 09:26:07 +0300

    chore: updating the assembly, auxiliary utilities and  dependencies

--- main-app/src/main/java/com/tricktracker/mainapp/Main.java
+++ main-app/src/main/java/com/tricktracker/mainapp/Main.java
@@ -2,8 +2,22 @@ package com.tricktracker.mainapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan; // для Spring Boot 4
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+        "com.tricktracker.mainapp",
+        "com.tricktracker.authservice",
+        "com.tricktracker.userservice" // <-- ДОБАВЛЕНО
+})
+@EnableJpaRepositories(basePackages = {
+        "com.tricktracker.authservice.repository",
+        "com.tricktracker.userservice.repository" // <-- ДОБАВЛЕНО
+})
+@EntityScan(basePackages = {
+        "com.tricktracker.authservice.entity",
+        "com.tricktracker.userservice.entity" // <-- ДОБАВЛЕНО
+})
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
